3b0bddb4fe225a0dfc42da2ab0be32ef3c9e45c7
erdemcicek <erdem25123@gmail>
2021-03-01 19:51:23 +0100

    int a

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -4,6 +4,8 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("I did commit");
 		System.out.println("Hello everyone");
+		int a = 5;
+		System.out.println(a);
 	}
 
 }
