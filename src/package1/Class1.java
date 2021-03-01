ff02ebcc000b78e09794757231d1c51d2ad96742
erdemcicek <erdem25123@gmail>
2021-03-01 21:28:36 +0100

    new comment

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -2,10 +2,20 @@ package package1;
 
 public class Class1 {
 	public static void main(String[] args) {
-		System.out.println("I did commit");
-		System.out.println("Hello everyone");
-		int a = 5;
-		System.out.println(a);
+//		System.out.println("I did commit");
+//		System.out.println("Hello everyone");
+//		int a = 5;
+//		System.out.println(a);
+		
+		int a[]= {5,4,6};
+		int b[]= {5,6,4};
+		boolean isEqual = true;
+		for ( int i = 0; i < a.length-1 ; i++) {
+			if ( a[i] != b[i]) {
+				isEqual = false;break;
+			}
+		}
+		System.out.println("Arrays are " + ((isEqual)?(""):("not ")) + "equal");
 	}
 
 }
